6d207c6dc79078f5f4a57495fad80ffb7952660c
gunaerode <guna.learn2016@gmail.com>
2022-02-26 01:57:59 +0530

    hooks: App.js file updated with hooks(reactv16.8 above will support) :snowflake::smiley:

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,56 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchField from '../components/SearchField';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 // import { robots } from './robots';
 
-class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      robots: [],
-      searchValue: '',
-      tempArray: [],
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [tempArray, setTempArray] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => {
         return response.json();
       })
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) => setRobots(users));
+  }, []); // [] will allow useEffect to run only once in render UI other vise app will render keep on
 
-  onSearchChange = (event) => {
-    const { robots } = this.state;
+  const onSearchChange = (event) => {
     const inputValue = event.target.value;
     const filterRobotsArray = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(inputValue.toLowerCase());
     });
-    this.setState({
-      tempArray: filterRobotsArray,
-    });
+    setTempArray(filterRobotsArray);
   };
 
-  render() {
-    const { robots, tempArray } = this.state;
-    return !robots.length ? (
-      <h1 style={{ textAlign: 'center' }}>Loading...</h1>
-    ) : (
-      <div className="container">
-        <h1>Robofriends</h1>
-        <SearchField searchChange={this.onSearchChange} />
-        <hr />
-        <Scroll>
-          <ErrorBoundry>
-            <CardList robots={tempArray.length > 0 ? tempArray : robots} />
-          </ErrorBoundry>
-        </Scroll>
-      </div>
-    );
-  }
+  return !robots.length ? (
+    <h1 style={{ textAlign: 'center' }}>Loading...</h1>
+  ) : (
+    <div className="container">
+      <h1>Robofriends</h1>
+      <SearchField searchChange={onSearchChange} />
+      <hr />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList robots={tempArray.length > 0 ? tempArray : robots} />
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
